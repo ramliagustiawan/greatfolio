08d4aa95d7076e31f9dbc7cab364ff25c8aeb024
ramliagustiawan <ramliagustiawan@gmail.com>
2023-02-26 11:18:56 +0800

    hero skill dan about

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ungu: "#8773EE",
+        kuning: "#FCDA68",
+        hitam: "#171513",
+        soft: "#717694",
+        mermud: "#EE73BD",
+        ijo: "#75EE73",
+      },
+    },
   },
   plugins: [],
 };
