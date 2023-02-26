18d415ff3421469688bcb92b29d5d0051cbf5000
ramliagustiawan <ramliagustiawan@gmail.com>
2023-02-26 20:23:40 +0800

    landing page selesai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         ungu: "#8773EE",
@@ -11,6 +16,14 @@ module.exports = {
         mermud: "#EE73BD",
         ijo: "#75EE73",
       },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+      body: ["Roboto", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
     },
   },
   plugins: [],
